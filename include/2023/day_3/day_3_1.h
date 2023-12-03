927072448eadcaff7830fb65904a0f249bfab52c
Indseta <55001697+Indseta@users.noreply.github.com>
2023-12-03 12:54:13 +0100

    Implement day 3.1

--- include/2023/day_3/day_3_1.h
+++ include/2023/day_3/day_3_1.h
@@ -3,7 +3,10 @@
 
 #include <base/day.h>
 
+#include <ctype.h>
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
 
 class Day3_1 : public Day {
@@ -13,5 +16,8 @@ class Day3_1 : public Day {
         void main() override;
 
     private:
-        void process_lines(const std::vector<std::string> &lines, int &sum);
+        void process_lines(std::vector<std::string> &lines, int &sum);
+
+        bool is_approved_symbol(const char &c);
+        bool is_adjacent(const std::vector<std::vector<int>> &symbol_locations, int x_location, int y_location, int x_size);
 };
--- src/2023/day_3/day_3_1.cpp
+++ src/2023/day_3/day_3_1.cpp
@@ -15,6 +15,77 @@ void Day3_1::main() {
 }
 
 
-void Day3_1::process_lines(const std::vector<std::string> &lines, int &sum) {
-    for (const auto &line : lines) {}
+void Day3_1::process_lines(std::vector<std::string> &lines, int &sum) {
+    std::vector<std::vector<int>> symbol_locations;
+
+    for (int line_i = 0; line_i < lines.size(); ++line_i) {
+        auto &line = lines[line_i];
+
+        for (int char_i = 0; char_i < line.size(); ++char_i) {
+            const char &c = line[char_i];
+            if (is_approved_symbol(c)) {
+                symbol_locations.push_back({char_i, line_i});
+            }
+        }
+    }
+
+    for (int line_i = 0; line_i < lines.size(); ++line_i) {
+        auto &line = lines[line_i];
+
+        int number = 0;
+        int start_index = 0;
+
+        for (int char_i = 0; char_i < line.size(); ++char_i) {
+            const char &c = line[char_i];
+            if (isdigit(c)) {
+                if (number == 0) {
+                    start_index = char_i;
+                }
+                number = number * 10 + (c - '0');
+            } else {
+                if (number) {
+                    if (is_adjacent(symbol_locations, start_index, line_i, char_i - start_index)) {
+                        sum += number;
+                    }
+                    number = 0;
+                }
+            }
+        }
+
+        if (number) {
+            if (is_adjacent(symbol_locations, start_index, line_i, line.size() - start_index)) {
+                sum += number;
+            }
+        }
+    }
+}
+
+
+bool Day3_1::is_approved_symbol(const char &c) {
+    if (c == '.' && c != '-') {
+        return false;
+    }
+    else if (isdigit(c)) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+
+bool Day3_1::is_adjacent(const std::vector<std::vector<int>> &symbol_locations, int x_location, int y_location, int x_size) {
+    for (int y = -1; y <= 1; ++y) {
+        for (int x = -1; x <= x_size; ++x) {
+            int current_x = x_location + x;
+            int current_y = y_location + y;
+
+            for (const auto &location : symbol_locations) {
+                if (location[0] == current_x && location[1] == current_y) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
